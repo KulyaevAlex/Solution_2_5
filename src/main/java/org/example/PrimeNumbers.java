14e8eb6cec7e416bacf68598a7afcb72417b67c5
KulyaevAlex <kavrzn86@gmial.com>
2026-09-05 18:06:41 +0300

    second commit

--- src/main/java/org/example/PrimeNumbers.java
+++ src/main/java/org/example/PrimeNumbers.java
@@ -1,18 +1,6 @@
 package org.example;
 
 public class PrimeNumbers {
-    public static void main(String[] args) {
-        int count = 0;
-        for (int i = 2; i <= 100; i++) {
-            if (checkPrime(i)) {
-                System.out.print(i + " ");
-                count++;
-            }
-        }
-        System.out.println();
-        System.out.println("Всего простых чисел: " + count);
-    }
-
     public static boolean checkPrime(int n) { //Метод определения простого числа
         if (n < 2) {
             return false;
@@ -24,5 +12,17 @@ public class PrimeNumbers {
             }
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 2; i <= 100; i++) {
+            if (checkPrime(i)) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.println("Всего простых чисел: " + count);
     }
 }
